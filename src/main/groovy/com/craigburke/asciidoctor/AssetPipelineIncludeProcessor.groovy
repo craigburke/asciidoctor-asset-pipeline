b4cc3ac0d9ec69a4c46aa52b32b33367071b8cf9
John Engelman <john.r.engelman@gmail.com>
2016-06-22 07:37:31 -0500

    Support tags on includes.

--- src/main/groovy/com/craigburke/asciidoctor/AssetPipelineIncludeProcessor.groovy
+++ src/main/groovy/com/craigburke/asciidoctor/AssetPipelineIncludeProcessor.groovy
@@ -23,8 +23,31 @@ class AssetPipelineIncludeProcessor extends IncludeProcessor {
         if (includeFile) {
             CacheManager.addCacheDependency(sourceFile, includeFile)
             String content = includeFile?.inputStream?.text ?: ''
-            reader.push_include(content, target, target, 1, attributes)
+            reader.push_include(preprocess(content, attributes), target, target, 1, attributes)
         }
+    }
+
+    private String preprocess(String content, Map<String, Object> attributes) {
+        StringBuilder sb = new StringBuilder()
+        if (attributes.containsKey('tags')) {
+            def tags = attributes.tags.split(/;|,/)
+            def lines = content.readLines()
+            tags.each { tag ->
+                def startTagIndex = lines.findIndexOf { it.contains("tag::${tag}[]") }
+                if (startTagIndex > -1) {
+                    def endTagIndex = lines.findIndexOf { it.contains("end::${tag}[]") }
+                    if (endTagIndex == -1) {
+                        throw new RuntimeException("No closing tag [${tag}] found in document!")
+                    }
+                    sb = sb.append(lines[(startTagIndex+1)..(endTagIndex-1)].join('\n'))
+                } else {
+                    throw new RuntimeException("Tag [${tag}] not found in document!")
+                }
+            }
+        } else {
+            sb = sb.append(content)
+        }
+        return sb.toString()
     }
 
 }
